566efbc4ac8da58ad1b894a20d75556606fe92f2
unknown <mhetrasaditya@gmail.com>
2023-04-30 17:37:40 +0530

    basic singleton

---
+++ LLD/ScalerLLD/src/main/java/com/scaler/lld/design/singleton/ConnectionPool.java
@@ -0,0 +1,18 @@
+package com.scaler.lld.design.singleton;
+
+public class ConnectionPool {
+    //step 3 - Instance variable
+    private ConnectionPool instance;
+
+    //step 1 - Hide the constructor
+    private ConnectionPool() {}
+
+    //step 2 - Global access point
+    public ConnectionPool getInstance() {
+        //step 4 - if present return, else create, set, return
+        if(instance == null) {
+            instance = new ConnectionPool();
+        }
+        return instance;
+    }
+}
