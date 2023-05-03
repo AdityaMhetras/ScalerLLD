9e8708929bab475052cc9baacf12b3e11cc28ce7
Aditya <mhetrasaditya@gmail.com>
2023-05-03 16:42:44 +0530

    Synchronized getter

--- LLD/ScalerLLD/src/main/java/com/scaler/lld/design/singleton/ConnectionPool.java
+++ LLD/ScalerLLD/src/main/java/com/scaler/lld/design/singleton/ConnectionPool.java
@@ -2,14 +2,17 @@ package com.scaler.lld.design.singleton;
 
 public class ConnectionPool {
     //step 3 - Instance variable
-    //eager creation
-    private static ConnectionPool instance = new ConnectionPool();
+    private static ConnectionPool instance;
 
     //step 1 - Hide the constructor
     private ConnectionPool() {}
 
     //step 2 - Global access point
-    public static ConnectionPool getInstance() {
+    public synchronized static ConnectionPool getInstance() {
+        //step 4 - if present return, else create, set, return
+        if(instance == null) {
+            instance = new ConnectionPool();
+        }
         return instance;
     }
 }
