e166a61c60822d954456b02d5a3cfd6f4c0c82d7
Aditya <mhetrasaditya@gmail.com>
2023-05-16 01:10:57 +0530

    Observer Pattern

--- LLD/ScalerLLD/src/main/java/com/scaler/lld/design/observer/interfaces/Publisher.java
+++ LLD/ScalerLLD/src/main/java/com/scaler/lld/design/observer/interfaces/Publisher.java
@@ -6,6 +6,7 @@ import java.util.List;
 //step 1 create interface of object of interest
 public abstract class Publisher {
 
+
     List<Observer> observers = new ArrayList<>();
     public void publish() {
         observers.forEach(Observer::consume);
